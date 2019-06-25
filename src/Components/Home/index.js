3531eddef93c80ddd9eafa5e20014f37fba8eb37
Adash <adash108@yahoo.com>
2019-06-25 17:33:37 +0100

    added empty listElement

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './home.css';
+
 const HomePage = () => {
 
   return (
---
+++ src/Components/List/ListElement.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './list.css';
+
+const ListElement = (props) => {
+
+  return (
+    <ul>
+      <li><p>one</p></li>
+      <li><p>two</p></li>
+      <li><p>three</p></li>
+      <li><p>four</p></li>
+    </ul>
+  )
+}
+export default ListElement
