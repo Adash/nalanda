f0f0e7c33ec6c6c6192fa7b6ee3fd57f2d294e5b
Adash <adash108@yahoo.com>
2019-11-15 16:52:07 +0000

    hiding whole navigation depending whether the user is logged in or not

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,28 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
 import SignOut from '../SignOut'
-
 import * as ROUTES from '../../Constants/routes'
-
+import { withAuthentication } from '../Session'
 import './navbar.css'
 
-const Navbar = () => {
-
+const Navbar = ({ user }) => {
   return (
     <div className="q_navbar">
       <h2 id="q_navbar_title"> Navbar </h2>
-      <div className="nav_links">
-        <Link to={ROUTES.LANDING} >Landing</Link>
-        <Link to={ROUTES.HOME} >Home</Link>
-        <Link to={ROUTES.POD} >Pod</Link>
-      </div>
-      <SignOut/>
-      <button className="lnk">
-        <Link to={ROUTES.SIGNIN}>Sign In</Link>
-      </button>
+      {user ? (
+        <>
+          <div className="nav_links">
+            <Link to={ROUTES.LANDING}>Landing</Link>
+            <Link to={ROUTES.HOME}>Home</Link>
+            <Link to={ROUTES.POD}>Pod</Link>
+          </div>
+          <SignOut />
+        </>
+      ) : (
+        <button className="lnk">
+          <Link to={ROUTES.SIGNIN}>Sign In</Link>
+        </button>
+      )}
     </div>
   )
 }
 
-export default Navbar;
+export default withAuthentication(Navbar)
